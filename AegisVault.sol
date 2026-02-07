f314235d46edd918fe4beaf6a0eaab53ec7d2697
ByteBreakeGhost <yasyfinance.9@gmail.com>
2026-02-07 09:21:47 +0700

    Update AegisVault.sol

--- AegisVault.sol
+++ AegisVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-prakma solidity ^0.8.20;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
