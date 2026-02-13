5aae7e17ff948e27b50c8a044d7dfb0bfccad765
ByteBreakeGhost <yasyfinance.9@gmail.com>
2026-02-13 09:47:15 +0700

    Update PriceConverter.sol

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -4,24 +4,14 @@ pragma solidity ^0.8.19;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-    function getPrice(AggregatorV3Interface priceFeed) 
-        internal 
-        view 
-        returns (uint256) 
-    {
-        (, int256 answer, , , ) = priceFeed.latestRoundData();
-        return uint256(answer * 1e10); // Convert to 18 decimals
-    }
-    
     function getConversionRate(
-        uint256 ethAmount, 
+        uint256 ethAmount,
         AggregatorV3Interface priceFeed
-    ) 
-        internal 
-        view 
-        returns (uint256) 
-    {
-        uint256 ethPrice = getPrice(priceFeed);
-        return (ethPrice * ethAmount) / 1e18;
+    ) internal view returns (uint256) {
+        (, int256 price, , uint256 updatedAt, ) = priceFeed.latestRoundData();
+        require(price > 0, "Invalid price");
+        require(block.timestamp - updatedAt < 2 hours, "Stale price");
+        
+        return (ethAmount * uint256(price)) / 1e18;
     }
 }
